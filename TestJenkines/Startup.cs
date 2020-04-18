07c2ff688e653a475241a5ec6dc02e53b8413ab1
Omar Salah <Omarsalahit2016@gmai.com>
2020-04-19 00:08:12 +0200

    test commit

--- TestJenkines/Startup.cs
+++ TestJenkines/Startup.cs
@@ -26,7 +26,7 @@ namespace TestJenkines
         {
             services.AddControllers();
         }
-
+        //new comment
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
