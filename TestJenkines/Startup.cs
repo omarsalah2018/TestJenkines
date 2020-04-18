56297b786a456ca0e5fb33387b0f06e89b2ba718
Omar Salah <Omarsalahit2016@gmai.com>
2020-04-19 00:52:18 +0200

    c

--- TestJenkines/Startup.cs
+++ TestJenkines/Startup.cs
@@ -23,6 +23,7 @@ namespace TestJenkines
         //new comment
         //sddsdsds
         //fdfdfd
+        //xcxxcx
         //sdsdsddssd
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
